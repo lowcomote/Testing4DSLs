e08e95de0b1fc46a1a03e41c11fb025d0fa1925f
Faezeh Khorram <khorram.faezeh@gmail.com>
2020-10-28 15:32:19 +0100

    deleting bin files

--- org.imt.k3tdl.k3dsa/xtend-gen/org/imt/k3tdl/k3dsa/AssertionAspectAssertionAspectProperties.java
+++
@@ -1,5 +0,0 @@
-package org.imt.k3tdl.k3dsa;
-
-@SuppressWarnings("all")
-public class AssertionAspectAssertionAspectProperties {
-}
--- org.imt.k3tdl.k3dsa/xtend-gen/org/imt/k3tdl/k3dsa/TDLRuntimeException.java
+++
@@ -1,8 +0,0 @@
-package org.imt.k3tdl.k3dsa;
-
-@SuppressWarnings("all")
-public class TDLRuntimeException extends Exception {
-  public TDLRuntimeException(final String message) {
-    super(message);
-  }
-}
