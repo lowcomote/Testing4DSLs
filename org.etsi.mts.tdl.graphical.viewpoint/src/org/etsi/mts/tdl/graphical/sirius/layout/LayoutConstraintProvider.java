51ffa601fdfd8df09bad063164509d80aa8ed485
Faezeh Khorram <khorram.faezeh@gmail.com>
2020-11-16 15:17:22 +0100

    delete files

--- org.etsi.mts.tdl.graphical.viewpoint/src/org/etsi/mts/tdl/graphical/sirius/layout/LayoutConstraintProvider.java
+++
@@ -1,7 +0,0 @@
-package org.etsi.mts.tdl.graphical.sirius.layout;
-
-import org.eclipse.draw2d.LayoutManager;
-
-public interface LayoutConstraintProvider {
-	Object getConstraint(LayoutManager layout);
-}
