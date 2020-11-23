2459fda30952096b3bb8ff5d4dbe2c08fd2dc823
Faezeh Khorram <khorram.faezeh@gmail.com>
2020-11-23 17:00:36 +0100

    Create TDLModelInstantiator.java

---
+++ org.imt.tdl.defaultPackage.generator/src/org/imt/tdl/defaultPackage/generator/TDLModelInstantiator.java
@@ -0,0 +1,9 @@
+package org.imt.tdl.defaultPackage.generator;
+
+public class TDLModelInstantiator {
+
+	public static void main(String[] args) {
+		String dslName = "BPMN";
+		TestDesignPackageGenerator tdpi = new TestDesignPackageGenerator(dslName);		
+	}
+}
