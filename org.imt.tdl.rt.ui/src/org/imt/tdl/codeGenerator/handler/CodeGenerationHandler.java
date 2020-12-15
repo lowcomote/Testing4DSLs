27fc209d22d7fc12692f7f0aab41b2409ee5c755
Faezeh Khorram <khorram.faezeh@gmail.com>
2020-12-15 14:00:25 +0100

    add new projects for eclipse actions and TDL constraints

---
+++ org.imt.tdl.rt.ui/src/org/imt/tdl/codeGenerator/handler/CodeGenerationHandler.java
@@ -0,0 +1,44 @@
+package org.imt.tdl.codeGenerator.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.window.Window;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.FileEditorInput;
+import org.imt.tdl.codeGenerator.ui.DSLSelectionWizard;
+import org.imt.tdl.codeGenerator.ui.DSLSelectionWizardPage;
+import org.imt.tdl.defaultPackage.generator.TDLCodeGenerator;
+
+public class CodeGenerationHandler extends AbstractHandler {
+	
+	private IWorkbenchWindow window;
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		try {
+			DSLSelectionWizard wizard = new DSLSelectionWizard();
+			WizardDialog dialog = new WizardDialog(Display.getDefault().getActiveShell(), wizard);
+			dialog.open();
+			DSLSelectionWizardPage page = (DSLSelectionWizardPage) wizard.getPage("SelectDSL");
+			String tdlProjectPath = page.getSelectedProjectPath().toString();
+			String dslFilePath = page.getSelectedDSLFilePath();
+			if (wizard.performFinish()) {
+				TDLCodeGenerator tdlCodeGenerator = new TDLCodeGenerator(dslFilePath, tdlProjectPath);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
---
+++ org.imt.tdl.rt.ui/src/org/imt/tdl/codeGenerator/ui/DSLSelectionWizardPage.java
@@ -0,0 +1,142 @@
+package org.imt.tdl.codeGenerator.ui;
+
+import java.awt.Font;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.gemoc.xdsmlframework.api.extensions.languages.LanguageDefinitionExtensionPoint;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+
+public class DSLSelectionWizardPage extends WizardPage{
+	private Composite parent;
+	private Group tdlProjectArea;
+	private Group languageArea;
+	
+	private org.eclipse.swt.widgets.Combo _projectCombo;
+	private org.eclipse.swt.widgets.Combo _languageCombo;
+	private IPath selectedProjectPath;
+	private String selectedDSLPath;
+	
+	protected DSLSelectionWizardPage(String pageName) {
+		super(pageName);
+		super.setTitle("Generate TDL code from DSL definition");
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		this.parent = parent;
+		Composite area = new Composite(parent, SWT.NULL);
+		GridLayout gl = new GridLayout(1, false);
+		gl.marginHeight = 0;
+		area.setLayout(gl);
+		area.layout();
+		setControl(area);
+		this.tdlProjectArea = createGroup(area, "Select a TDL Project");
+		this.languageArea = createGroup(area, "Select a language");
+		createProjectLayout (tdlProjectArea, null);
+		createLanguageLayout (languageArea, null);
+	}
+	protected Group createGroup(Composite parent, String text) {
+		Group group = new Group(parent, SWT.NULL);
+		group.setText(text);
+		GridLayout locationLayout = new GridLayout();
+		locationLayout.numColumns = 3;
+		locationLayout.marginHeight = 10;
+		locationLayout.marginWidth = 10;
+		group.setLayout(locationLayout);
+		return group;
+	}
+	public Composite createProjectLayout(Composite parent, Font font) {
+		createTextLabelLayout(parent, "TDL Projects");
+		_projectCombo = new Combo(parent, SWT.NONE);
+		_projectCombo.setLayoutData(createStandardLayout());
+
+		Set<String> projectNames = (Set<String>) getAllTDLProjects().keySet();
+		String[] empty = {};
+		_projectCombo.setItems(projectNames.toArray(empty));
+		_projectCombo.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				selectedProjectPath = getAllTDLProjects().get(_projectCombo.getText());
+			}
+		});	
+		createTextLabelLayout(parent, "");
+		return parent;
+	}
+	public Composite createLanguageLayout(Composite parent, Font font) {
+		// Language
+		createTextLabelLayout(parent, "Languages");
+		_languageCombo = new Combo(parent, SWT.NONE);
+		_languageCombo.setLayoutData(createStandardLayout());
+
+		Set<String> languagesNames = (Set<String>) getAllLanguages().keySet();
+		String[] empty = {};
+		_languageCombo.setItems(languagesNames.toArray(empty));
+		_languageCombo.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				selectedDSLPath = getAllLanguages().get(_languageCombo.getText());
+			}
+		});
+		
+		createTextLabelLayout(parent, "");
+
+		return parent;
+	}
+	protected void createTextLabelLayout(Composite parent, String labelString) {
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		parent.setLayoutData(gd);
+		Label inputLabel = new Label(parent, SWT.NONE);
+		inputLabel.setText(labelString); // $NON-NLS-1$
+	}
+	private GridData createStandardLayout() {
+		return new GridData(SWT.FILL, SWT.CENTER, true, false);
+	}
+	public HashMap<String, String> getAllLanguages() {
+		HashMap<String, String> languagesPaths = new HashMap<String, String>();
+
+		IConfigurationElement[] languages = Platform
+				.getExtensionRegistry().getConfigurationElementsFor(
+						LanguageDefinitionExtensionPoint.GEMOC_LANGUAGE_EXTENSION_POINT);
+		for (IConfigurationElement lang : languages) {
+			String xdsmlPath = lang.getAttribute("xdsmlFilePath");
+			String xdsmlName = lang.getAttribute("name");
+			if (xdsmlPath.endsWith(".dsl")) {
+				languagesPaths.put(xdsmlName, xdsmlPath);
+			}
+		}
+		return languagesPaths;
+	}
+	public HashMap<String, IPath> getAllTDLProjects() {
+		HashMap<String, IPath> projects = new HashMap<>();
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IProject[] tdlProjects = root.getProjects();
+		for (int i=0; i<tdlProjects.length; i++) {
+			projects.put(tdlProjects[i].getName(), tdlProjects[i].getFullPath());
+		}
+		return projects;
+	}
+	public IPath getSelectedProjectPath() {
+		return this.selectedProjectPath;
+	}
+	public String getSelectedDSLFilePath() {
+		return this.selectedDSLPath;
+	}
+}
