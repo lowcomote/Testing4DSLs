d8328e9f5c69b521d124bb1b6b4c134654693aa9
Faezeh Khorram <khorram.faezeh@gmail.com>
2021-11-01 21:59:59 +0100

    measure test suite coverage

--- org.imt.tdl.coverage/src/org/imt/tdl/coverage/TDLTestSuiteCoverage.java
+++ org.imt.tdl.coverage/src/org/imt/tdl/coverage/TDLTestSuiteCoverage.java
@@ -1,11 +1,18 @@
 package org.imt.tdl.coverage;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
 
 public class TDLTestSuiteCoverage {
 
 	private List<TDLTestCaseCoverage> tcCoverages = new ArrayList<>();
+	
+	public HashMap<EObject, String> tsObjectCoverageStatus = TDLCoverageUtil.getInstance().objectCoverageStatus;
+	private double tsCoveragePercentage;
 	
 	//for every test case of the test suite, add its coverage to the list
 	public void addTCCoverage(TDLTestCaseCoverage tcCoverage) {
@@ -17,12 +24,26 @@ public class TDLTestSuiteCoverage {
 	
 	//Calculating coverage of the test suite based on its test cases coverage
 	public void calculateTSCoverage() {
-		calculateTCCoverage();
+		for (TDLTestCaseCoverage tcCoverage : tcCoverages) {
+			//we send the tsObjectCoverageStatus to calculate the test suite coverage while calculating the test cases coverages
+			tcCoverage.calculateTCCoverage(tsObjectCoverageStatus);
+		}
+		
+		int numOfCoveredObjs = 0;
+		Iterator<String> statusValues = tsObjectCoverageStatus.values().iterator();
+		while (statusValues.hasNext()) {
+			if (statusValues.next() == TDLCoverageUtil.COVERED) {
+				numOfCoveredObjs++;
+			}
+		}
+
+		this.tsCoveragePercentage = (numOfCoveredObjs*100)/TDLCoverageUtil.getInstance().getModelSize();
 	}
 	
-	private void calculateTCCoverage() {
-		for (TDLTestCaseCoverage tcCoverage : tcCoverages) {
-			tcCoverage.calculateTCCoverage();
-		}
+	public double getTsCoveragePercentage() {
+		return tsCoveragePercentage;
+	}
+	public void setTsCoveragePercentage(double coveragePercentage) {
+		this.tsCoveragePercentage = coveragePercentage;
 	}
 }
