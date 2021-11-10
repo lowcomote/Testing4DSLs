b95e52a6f63c6b09d71188a5338d2296abe7eeae
Faezeh Khorram <khorram.faezeh@gmail.com>
2021-11-10 11:21:04 +0100

    create a project for SBFL

---
+++ org.imt.tdl.faultLocalization/src/org/imt/tdl/faultLocalization/ModelElementSuspiciousness.java
@@ -0,0 +1,50 @@
+package org.imt.tdl.faultLocalization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.imt.tdl.coverage.TDLCoverageUtil;
+import org.imt.tdl.coverage.TDLTestSuiteCoverage;
+import org.imt.tdl.coverage.TestCoverageInfo;
+import org.imt.tdl.testResult.TDLTestCaseResult;
+import org.imt.tdl.testResult.TDLTestPackageResult;
+import org.imt.tdl.testResult.TestResultUtil;
+
+public class ModelElementSuspiciousness {
+	
+	private final TDLTestPackageResult testSuiteResult;
+	private final List<TDLTestCaseResult> errorVector;
+	private final TDLTestSuiteCoverage testSuiteCoverage;
+	private final List<TestCoverageInfo> coverageMatix;
+	
+	public ModelElementSuspiciousness() {
+		this.testSuiteResult = TestResultUtil.getInstance().getTestPackageResult();
+		this.errorVector = this.testSuiteResult.getResults();
+		this.testSuiteCoverage = TDLCoverageUtil.getInstance().getTestSuiteCoverage();
+		this.coverageMatix = this.testSuiteCoverage.coverageInfos;
+		//the last row of the matrix contains coverage percentages which is not required here, so should be removed 
+		this.coverageMatix.remove(this.coverageMatix.size()-1);
+	}
+	
+	private void calculateSuspiciousness() {
+		for (int i=0; i<coverageMatix.size(); i++) {
+			EObject modelElement = coverageMatix.get(i).getModelObject();
+			ArrayList<String> elementCoverageStatus = coverageMatix.get(i).getCoverage();
+			//if the element is not coverable, remove it from the matrix
+			if (elementCoverageStatus.get(elementCoverageStatus.size()-1) == TDLCoverageUtil.NOT_COVERABLE) {
+				coverageMatix.remove(i);
+			}else {
+				SBFLOperandInfo operands4modelElement = new SBFLOperandInfo();
+				operands4modelElement.setModelObject(modelElement);
+				operands4modelElement.setMetaclass(modelElement.eClass());
+				operands4modelElement.setNF(this.testSuiteResult.getNumOfFailedTestCases());
+				operands4modelElement.setNS(this.testSuiteResult.getNumOfPassedTestCases());
+			}		
+		}
+	}
+	
+	private int calculateNCF(int index) {
+		return 0;
+	}
+}
---
+++ org.imt.tdl.faultLocalization/src/org/imt/tdl/faultLocalization/SBFLOperandInfo.java
@@ -0,0 +1,93 @@
+package org.imt.tdl.faultLocalization;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+
+public class SBFLOperandInfo {
+
+	private EClass metaclass;
+	private EObject modelObject;
+	private int NCF;//number of failed test cases that cover a coverable model element
+	private int NUF;//number of failed test cases that do not cover a coverable model element
+	private int NCS;//number of successful test cases that cover a coverable model element 
+	private int NUS;//number of successful test cases that do not cover a coverable model element 
+	private int NC;//total number of test cases that cover a coverable model element
+	private int NU;//total number of test cases that do not cover a coverable model element 
+	private int NS;//total number of successful test cases
+	private int NF;//total number of failed test cases
+	private double Susp;//Suspiciousness score
+	private int rank;//Suspiciousness rank
+	
+	public EClass getMetaclass() {
+		return metaclass;
+	}
+	public void setMetaclass(EClass metaclass) {
+		this.metaclass = metaclass;
+	}
+	public EObject getModelObject() {
+		return modelObject;
+	}
+	public void setModelObject(EObject modelObject) {
+		this.modelObject = modelObject;
+	}
+	public int getNCF() {
+		return NCF;
+	}
+	public void setNCF(int nCF) {
+		NCF = nCF;
+	}
+	public int getNUF() {
+		return NUF;
+	}
+	public void setNUF(int nUF) {
+		NUF = nUF;
+	}
+	public int getNCS() {
+		return NCS;
+	}
+	public void setNCS(int nCS) {
+		NCS = nCS;
+	}
+	public int getNUS() {
+		return NUS;
+	}
+	public void setNUS(int nUS) {
+		NUS = nUS;
+	}
+	public int getNC() {
+		return NC;
+	}
+	public void setNC(int nC) {
+		NC = nC;
+	}
+	public int getNU() {
+		return NU;
+	}
+	public void setNU(int nU) {
+		NU = nU;
+	}
+	public int getNS() {
+		return NS;
+	}
+	public void setNS(int nS) {
+		NS = nS;
+	}
+	public int getNF() {
+		return NF;
+	}
+	public void setNF(int nF) {
+		NF = nF;
+	}
+	public double getSusp() {
+		return Susp;
+	}
+	public void setSusp(double susp) {
+		Susp = susp;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+}
