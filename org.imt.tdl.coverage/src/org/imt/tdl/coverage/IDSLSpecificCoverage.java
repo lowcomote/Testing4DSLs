1f88f9404968e198dffa17d873759cdd9e6b0bf5
Faezeh Khorram <khorram.faezeh@gmail.com>
2021-11-18 16:30:21 +0100

    add a new method for updating the coverable classes

--- org.imt.tdl.coverage/src/org/imt/tdl/coverage/IDSLSpecificCoverage.java
+++ org.imt.tdl.coverage/src/org/imt/tdl/coverage/IDSLSpecificCoverage.java
@@ -1,5 +1,6 @@
 package org.imt.tdl.coverage;
 
 public interface IDSLSpecificCoverage {
+	public void updateCoverableClasses();
 	public void specializeCoverage(TDLTestCaseCoverage testCaseCoverage);
 }
