d5ca279d24c28fd02be0ab7629bc791ff56a13b2
Faezeh Khorram <khorram.faezeh@gmail.com>
2021-12-01 13:54:36 +0100

    add a new method to enable ignoring some model objects in coverage computation

--- org.imt.tdl.coverage/src/org/imt/tdl/coverage/IDSLSpecificCoverage.java
+++ org.imt.tdl.coverage/src/org/imt/tdl/coverage/IDSLSpecificCoverage.java
@@ -2,7 +2,10 @@ package org.imt.tdl.coverage;
 
 import java.util.List;
 
+import org.eclipse.emf.ecore.resource.Resource;
+
 public interface IDSLSpecificCoverage {
-	public List<Class> getNewCoverableClasses();
+	public List<String> getNewCoverableClasses();
+	public void ignoreModelObjects(Resource MUTResource);
 	public void specializeCoverage(TDLTestCaseCoverage testCaseCoverage);
 }
