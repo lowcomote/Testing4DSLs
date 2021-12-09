9228d885130554404c4d89a4d3b63d6b62efe30b
Faezeh Khorram <khorram.faezeh@gmail.com>
2021-12-09 16:25:22 +0100

    Update TDLCore.java

--- org.imt.tdl.runner/src/org/imt/tdl/runner/TDLCore.java
+++ org.imt.tdl.runner/src/org/imt/tdl/runner/TDLCore.java
@@ -1,43 +1,15 @@
 package org.imt.tdl.runner;
 
 import org.etsi.mts.tdl.Package;
+
 import org.etsi.mts.tdl.TestDescription;
 import org.imt.k3tdl.k3dsa.TestConfigurationAspect;
 import org.imt.k3tdl.k3dsa.TestDescriptionAspect;
-import org.imt.tdl.coverage.TDLCoverageUtil;
-import org.imt.tdl.coverage.TDLTestCaseCoverage;
-import org.imt.tdl.coverage.TDLTestSuiteCoverage;
 import org.imt.tdl.testResult.TDLTestCaseResult;
 import org.imt.tdl.testResult.TDLTestResultUtil;
 import org.imt.tdl.testResult.TDLTestSuiteResult;
 
 public class TDLCore {
-
-	TDLTestSuiteResult testSuiteResult = new TDLTestSuiteResult();
-	TDLTestSuiteCoverage testSuiteCoverage = new TDLTestSuiteCoverage();
-	String DSLPath = "";
-	
-	public void runTestAndCalculateCoverage(Package testPackage, String artifactPath) {
-		artifactPath = artifactPath.replace("\\", "/");
-		for (int i=0; i<testPackage.getPackagedElement().size(); i++) {
-			Object o = testPackage.getPackagedElement().get(i);
-			if (o instanceof TestDescription) {
-				TestDescription testCase = (TestDescription) o;
-				TestDescriptionAspect.executeTestCase(testCase, artifactPath);
-				TDLTestCaseResult testCaseResult = TestDescriptionAspect.testCaseResult(testCase);
-				TDLTestCaseCoverage testCaseCoverage = TestDescriptionAspect.testCaseCoverage(testCase);
-				this.testSuiteResult.addResult(testCaseResult);
-				this.testSuiteCoverage.addTCCoverage(testCaseCoverage);
-				if (this.DSLPath == "") {
-					this.DSLPath = TestConfigurationAspect.DSLPath(testCase.getTestConfiguration());
-				}
-			}
-			TDLTestResultUtil.getInstance().setTestSuiteResult(this.testSuiteResult);
-		    TDLCoverageUtil.getInstance().setTestSuiteCoverage(this.testSuiteCoverage);
-		    TDLCoverageUtil.getInstance().setDSLPath(this.DSLPath);
-		    TDLCoverageUtil.getInstance().runCoverageComputation();
-		}
-	}
 	
 	//this method is called from WODEL tool
 	public Result run(Package testPackage, String artifactPath) {
@@ -63,21 +35,4 @@ public class TDLCore {
 		}
 		return result;
 	}
-
-	public TDLTestSuiteResult getTestSuiteResult() {
-		return testSuiteResult;
-	}
-
-	public void setTestSuiteResult(TDLTestSuiteResult testSuiteResult) {
-		this.testSuiteResult = testSuiteResult;
-	}
-
-	public TDLTestSuiteCoverage getTestSuiteCoverage() {
-		return testSuiteCoverage;
-	}
-
-	public void setTestSuiteCoverage(TDLTestSuiteCoverage testSuiteCoverage) {
-		this.testSuiteCoverage = testSuiteCoverage;
-	}
-	
 }
