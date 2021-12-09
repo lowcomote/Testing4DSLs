4dbe432c64008b9aa0bf195663e19d80c7192c5c
Faezeh Khorram <khorram.faezeh@gmail.com>
2021-12-09 19:30:15 +0100

    mimic functionality of CodeCover tool

--- Language_Workbench/xMiniJava/org.imt.xminijava.coverage/src/org/imt/xminijava/coverage/MiniJavaCoverageComputation.java
+++ Language_Workbench/xMiniJava/org.imt.xminijava.coverage/src/org/imt/xminijava/coverage/MiniJavaCoverageComputation.java
@@ -9,12 +9,17 @@ import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.imt.minijava.xminiJava.ArrayTypeRef;
+import org.imt.minijava.xminiJava.Block;
 import org.imt.minijava.xminiJava.Class;
+import org.imt.minijava.xminiJava.ForStatement;
+import org.imt.minijava.xminiJava.IfStatement;
 import org.imt.minijava.xminiJava.Member;
 import org.imt.minijava.xminiJava.Method;
+import org.imt.minijava.xminiJava.MethodCall;
 import org.imt.minijava.xminiJava.Program;
 import org.imt.minijava.xminiJava.StringTypeRef;
 import org.imt.minijava.xminiJava.TypeDeclaration;
+import org.imt.minijava.xminiJava.WhileStatement;
 import org.imt.tdl.coverage.IDSLSpecificCoverage;
 import org.imt.tdl.coverage.TDLCoverageUtil;
 import org.imt.tdl.coverage.TDLTestCaseCoverage;
@@ -41,30 +46,75 @@ public class MiniJavaCoverageComputation implements IDSLSpecificCoverage{
 			if (modelObject instanceof Program) {
 				programCoverage ((Program) modelObject);
 			}
-			else if (modelObject instanceof Class && coverage != TDLCoverageUtil.COVERED) {
-				classCoverage ((Class) modelObject);
-			}
 			else if (modelObject instanceof Method) {
 				methodCoverage ((Method) modelObject);
+			}
+			else if (modelObject instanceof MethodCall) {
+				methodCallCoverage ((MethodCall) modelObject);
+			}
+//			else if (modelObject instanceof Class && coverage != TDLCoverageUtil.COVERED) {
+//				classCoverage ((Class) modelObject);
+//			}
+			else if (modelObject instanceof IfStatement) {
+				ifCoverage ((IfStatement) modelObject);
+			}
+			else if (modelObject instanceof WhileStatement) {
+				whileCoverage ((WhileStatement) modelObject);
+			}
+			else if (modelObject instanceof ForStatement) {
+				forCoverage ((ForStatement) modelObject);
 			}
 		}
 	}
 
+	private void methodCallCoverage(MethodCall modelObject) {
+		//if the container of a methodCall is a block, it is indeed a statement and its coverage status must be considered
+		//but if it is contained by other elements such as if, while, ..., it is not coverable
+		MethodCall methodCall = (MethodCall) modelObject;
+		if (!(methodCall.eContainer() instanceof Block)) {
+			int index = this.modelObjects.indexOf(methodCall);
+			this.testCaseCoverage.tcObjectCoverageStatus.set(index, TDLCoverageUtil.NOT_COVERABLE);
+		}
+	}
+
+	private void forCoverage(ForStatement forStatement) {
+		//for statement coverage, for statements should not be considered in the coverage computation
+		//only its inner instructions/assignments are considered
+		int index = this.modelObjects.indexOf(forStatement);
+		this.testCaseCoverage.tcObjectCoverageStatus.set(index, TDLCoverageUtil.NOT_COVERABLE);
+	}
+
+	private void whileCoverage(WhileStatement whileStatement) {
+		//for statement coverage, while statements should not be considered in the coverage computation
+		//only its inner instructions/assignments are considered
+		int index = this.modelObjects.indexOf(whileStatement);
+		this.testCaseCoverage.tcObjectCoverageStatus.set(index, TDLCoverageUtil.NOT_COVERABLE);
+	}
+
+	private void ifCoverage(IfStatement ifStatement) {
+		//for statement coverage, if statements should not be considered in the coverage computation
+		//only its inner instructions/assignments are considered
+		int index = this.modelObjects.indexOf(ifStatement);
+		this.testCaseCoverage.tcObjectCoverageStatus.set(index, TDLCoverageUtil.NOT_COVERABLE);
+	}
+
 	private void methodCoverage(Method method) {
-		//to be compatible with JaCoCo, a method signature should not be considered in the coverage computation
+		//for statement coverage, a method signature should not be considered in the coverage computation
 		//only its inner instructions/assignments are considered
 		int index = this.modelObjects.indexOf(method);
 		this.testCaseCoverage.tcObjectCoverageStatus.set(index, TDLCoverageUtil.NOT_COVERABLE);
 	}
 
 	private void programCoverage(Program program) {
-		//to be compatible with JaCoCo, a program should not be considered in the coverage computation
+		//for statement coverage, program should not be considered in the coverage computation
+		//only its inner instructions/assignments are considered
 		int index = this.modelObjects.indexOf(program);
 		this.testCaseCoverage.tcObjectCoverageStatus.set(index, TDLCoverageUtil.NOT_COVERABLE);
 	}
 
 	private void classCoverage(Class clazz) {
 		//a class is covered when at least one of its methods is covered
+		//this method is useful when we need to have class coverage
 		int clazzIndex = this.modelObjects.indexOf(clazz);
 		for (Member member: clazz.getMembers()) {
 			if (member instanceof Method) {
