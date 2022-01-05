2eae6668995b3bd7cabb26610a8c72ea8e19be0f
Faezeh Khorram <khorram.faezeh@gmail.com>
2022-01-05 17:36:22 +0100

    run evaluation on a job

--- org.imt.tdl.sbfl.evaluation/src/org/imt/tdl/sbfl/evaluation/ui/SBFLEvaluationHandler.java
+++ org.imt.tdl.sbfl.evaluation/src/org/imt/tdl/sbfl/evaluation/ui/SBFLEvaluationHandler.java
@@ -3,6 +3,9 @@ package org.imt.tdl.sbfl.evaluation.ui;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
 import org.imt.tdl.sbfl.evaluation.SBFLEvaluation;
@@ -17,8 +20,15 @@ public class SBFLEvaluationHandler extends AbstractHandler {
 			dialog.open();
 			ProjectSelectionWizardPage page = (ProjectSelectionWizardPage) wizard.getPage("selectProject");
 			if (wizard.performFinish()) {
-				SBFLEvaluation evaluation = new SBFLEvaluation(page.getMutantProject(), page.getTestProject());
-				evaluation.evaluateSBFLTechniques();
+				Job job = new Job("SBFLEvaluation") {
+					@Override
+					protected IStatus run(IProgressMonitor monitor) {
+						SBFLEvaluation evaluation = new SBFLEvaluation(page.getMutantProject(), page.getTestProject());
+						evaluation.evaluateSBFLTechniques();
+						return null;
+					}
+				};
+				job.schedule();
 			}
 		}catch (Exception e) {
 			e.printStackTrace();
