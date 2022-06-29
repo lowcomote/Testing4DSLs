16b6414792778e5536f067bdb8b6ec914046390f
Faezeh Khorram <khorram.faezeh@gmail.com>
2022-06-29 18:18:37 +0200

    add more functionalities

--- org.imt.tdl.coverage/src/org/imt/tdl/coverage/dslSpecific/DSLSpecificCoverageRule.java
+++ org.imt.tdl.coverage/src/org/imt/tdl/coverage/dslSpecific/DSLSpecificCoverageRule.java
@@ -1,5 +1,6 @@
 package org.imt.tdl.coverage.dslSpecific;
 
+import org.eclipse.emf.common.util.ECollections;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EReference;
@@ -9,26 +10,80 @@ public class DSLSpecificCoverageRule {
 	
 	private EClass context;
 	//?? maybe a map would be needed
-	private EList<EReference> impliesReferenceCoverage;
-
+	private EList<EReference> referenceCoverage;
+	private EList<EReference> containerCoverageByAll;
+	private EList<EReference> containerCoverageByOne;
+	
+	private boolean ignoreClassFromCoverage;
+	
 	public EClass getContext() {
 		return context;
 	}
 	public void setContext(EClass context) {
 		this.context = context;
 	}
-	public EList<EReference> getImpliesReferenceCoverage() {
-		return impliesReferenceCoverage;
-	}
-	public void setImpliesReferenceCoverage(EList<EReference> impliesReferenceCoverage) {
-		this.impliesReferenceCoverage = impliesReferenceCoverage;
-		impliesReferenceCoverage.forEach(r -> updateCoverableClasses(r));
+	
+	public EList<EReference> getReferenceCoverage() {
+		return referenceCoverage;
 	}
 	
-	private void updateCoverableClasses(EReference r) {
+	//from the coverage of the context class, the coverage of its referenced classes can be implied
+	public void setReferenceCoverage(EList<EReference> referenceCoverage) {
+		this.referenceCoverage = referenceCoverage;
+		this.referenceCoverage.forEach(r -> addNewCoverableClass(r));
+	}
+	
+	public void setReferenceCoverage(EReference referenceCoverage) {
+		this.referenceCoverage = ECollections.asEList(referenceCoverage);
+		addNewCoverableClass(referenceCoverage);
+	}
+	
+	//from the coverage of the context class, the coverage of its container class can be implied
+	public EList<EReference> getContainerCoverageByAll() {
+		return containerCoverageByAll;
+	}
+	public void setContainerCoverageByAll(EList<EReference> containerCoverageByAll) {
+		this.containerCoverageByAll = containerCoverageByAll;
+		this.containerCoverageByAll.forEach(r -> addNewCoverableClass((EClass) r.eContainer()));
+	}
+	public void setContainerCoverageByAll(EReference containerCoverageByAll) {
+		this.containerCoverageByAll = ECollections.asEList(containerCoverageByAll);
+		addNewCoverableClass((EClass) containerCoverageByAll.eContainer());
+	}
+	public EList<EReference> getContainerCoverageByOne() {
+		return containerCoverageByOne;
+	}
+	public void setContainerCoverageByOne(EList<EReference> containerCoverageByOne) {
+		this.containerCoverageByOne = containerCoverageByOne;
+		this.containerCoverageByOne.forEach(r -> addNewCoverableClass((EClass) r.eContainer()));
+	}
+	public void setContainerCoverageByOne(EReference containerCoverageByOne) {
+		this.containerCoverageByOne = ECollections.asEList(containerCoverageByOne);
+		addNewCoverableClass((EClass) containerCoverageByOne.eContainer());
+	}
+	private void addNewCoverableClass(EReference r) {
 		if (!TDLCoverageUtil.getInstance().isClassCoverable((EClass) r.getEType())) {
 			TDLCoverageUtil.getInstance().addNewCoverableClass((EClass) r.getEType());
 		}
 	}
 	
+	private void addNewCoverableClass(EClass c) {
+		if (!TDLCoverageUtil.getInstance().isClassCoverable(c)) {
+			TDLCoverageUtil.getInstance().addNewCoverableClass(c);
+		}
+	}
+	
+	public void ignoreClassFromCoverage() {
+		ignoreClassFromCoverage = true;
+		if (TDLCoverageUtil.getInstance().isClassCoverable(context)) {
+			TDLCoverageUtil.getInstance().removeCoverableClass(context);
+		}
+	}
+	
+	public void ignoreClass_subClassesFromCoverage() {
+		ignoreClassFromCoverage = true;
+		if (TDLCoverageUtil.getInstance().isClassCoverable(context)) {
+			TDLCoverageUtil.getInstance().removeCoverableClass_subClass(context);
+		}
+	}
 }
