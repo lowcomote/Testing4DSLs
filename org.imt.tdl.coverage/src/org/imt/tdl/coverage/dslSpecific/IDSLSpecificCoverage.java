54d0e70dbd81277bf132026b98684d0c3ce94850
Faezeh Khorram <khorram.faezeh@gmail.com>
2022-07-20 15:38:35 +0200

    Update IDSLSpecificCoverage.java

--- org.imt.tdl.coverage/src/org/imt/tdl/coverage/dslSpecific/IDSLSpecificCoverage.java
+++ org.imt.tdl.coverage/src/org/imt/tdl/coverage/dslSpecific/IDSLSpecificCoverage.java
@@ -1,11 +1,12 @@
 package org.imt.tdl.coverage.dslSpecific;
 
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.resource.Resource;
+
+import DSLSpecificCoverage.DomainSpecificCoverage;
 
 public interface IDSLSpecificCoverage {
 	
-	public EList<DSLSpecificCoverageRule> getDSLSpecificCoverageRules();
+	public DomainSpecificCoverage getDomainSpecificCoverage();
 	public void ignoreModelObjects(Resource MUTResource);
 	
 }
