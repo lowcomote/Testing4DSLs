4a2c15a772ec294cb7faf768d18ac846e4d7e228
Faezeh Khorram <khorram.faezeh@gmail.com>
2022-09-10 20:29:07 +0200

    Update Result.java

--- testing_framework/test_engine/org.imt.tdl.runner/src/org/imt/tdl/runner/Result.java
+++ testing_framework/test_engine/org.imt.tdl.runner/src/org/imt/tdl/runner/Result.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.etsi.mts.tdl.TestDescription;
 import org.imt.tdl.testResult.TDLTestCaseResult;
 
 public class Result {
